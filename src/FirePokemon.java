4534a1d41510c99f03aa16429fa58484cc681271
AdriaandeWit <adriaandewit3@gmail.com>
2023-01-15 13:53:12 +0100

    first commit

--- src/FirePokemon.java
+++ src/FirePokemon.java
@@ -4,7 +4,6 @@ import java.util.List;
 public class FirePokemon extends Pokemon{
 
     List<String> attacks = Arrays.asList("infero","pyroball", "firelash","flameThrower");
-    List<Integer> attacksValue = Arrays.
     public FirePokemon(String name, int level, int hp, String food, String sounds) {
         super(name , level , hp , food , sounds,"Fire");
     }
@@ -13,13 +12,31 @@ public class FirePokemon extends Pokemon{
         int attackValueInferno = 60;
 
         System.out.println(pokemon.getName() + "Attacks" + gymPokemon.getName() + "with inferno");
+
+
         }
 
     }
     public void pyroBall(Pokemon pokemon, Pokemon gymPokemon) {
         int attackValuePyroBall = 80;
+        int divdeWater = attackValuePyroBall/10*8;
+        int divdeElectric = attackValuePyroBall/10*6;
+        int divdeFire = attackValuePyroBall/10+6;
         System.out.println(pokemon.getName() + "Attacks" + gymPokemon.getName() + "with pyro ball");
-        gymPokemon.getHp();
+
+        if (gymPokemon.getType().equals("Grass")) {
+            System.out.println("is full affective on " + gymPokemon.getType());
+            System.out.println(gymPokemon.getHp()+ "there go 80 HP off "+ attackValuePyroBall-gymPokemon.getHp() );
+        }
+        if (gymPokemon.getType().equals("Water")) {
+            System.out.println(gymPokemon.getType() + "so that means that the attack is not full affective" + gymPokemon + "loses: ");
+            System.out.println(gymPokemon.getHp()+ "there go 64 HP off "+ divdeWater-gymPokemon.getHp() );
+        }
+        if (gymPokemon.getType().equals("electric")) {
+
+
+        } else {
+        }
     }
     public void fireLash(Pokemon pokemon, Pokemon gymPokemon) {
         int attackValueFireLash = 50;
@@ -28,29 +45,6 @@ public class FirePokemon extends Pokemon{
         public void flameThrower(Pokemon pokemon, Pokemon gymPokemon) {
         int attackValueFlameThrower = 70;
         System.out.println(pokemon.getName() + "Attacks" + gymPokemon.getName() + "with flame thrower");
-    }
-    public int logicFire( ){
-
-
-
-        if(gymPokemon.getType().equals("Grass")){
-           attacks = pr
-        }
-        if (gymPokemon.getType().equals("Water")){
-            avl
-            System.out.println(gymPokemon.getType() + "so that means that the attack is not full affective" + gymPokemon + "loses: " );
-        }
-        if (gymPokemon.getType().equals("electric")){
-
-        }
-        else{
-
-        }
-
-
-
-
-
     }
     @Override
     public List<String> getAttacks() {
